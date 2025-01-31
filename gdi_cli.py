48dc54ce4eeb7e47f4a355b2c8ec1729294e97ef
JoelDJ2002 <jdjferrari@gmail.com>
2025-01-31 23:29:31 +0530

    created buffer feature

--- gdi_cli.py
+++ gdi_cli.py
@@ -3,6 +3,7 @@ from auth.token_gen import TokenGenerator
 from features.get_re import ResourceFetcher
 from features.count_features import count_features
 from features.download_features import download_features
+from features.buffer import make_buffer
 from common.minio_ops import get_ls
 
 def gen_token(client_id, client_secret, role):
@@ -49,10 +50,10 @@ def fetch_resource(client_id, client_secret, role, resource_id, save_object, con
 @click.command()
 @click.option('--config', required=True, help="Path to the config file.")
 @click.option('--client-id', required=True, help="used as the bucket name.")
-@click.option('--artefact-url', required=True, help="URL of the artefact to count features.")
-def features_count(config, client_id, artefact_url):
-    """Count the features in the artefact."""
-    count = count_features(config, client_id, artefact_url)
+@click.option('--artifact-url', required=True, help="URL of the artifact to count features.")
+def features_count(config, client_id, artifact_url):
+    """Count the features in the artifact."""
+    count = count_features(config, client_id, artifact_url)
     click.echo(f"Feature Count: {count}")
 
 
@@ -67,8 +68,20 @@ def ls_objects(config, client_id):
 @click.command()
 @click.option('--config', required=True, help="Path to the config file.")
 @click.option('--client-id', required=True, help="Client ID for authentication.")
-@click.option('--artefact-url', required=True, help="URL of the artefact to download.")
+@click.option('--artifact-url', required=True, help="URL of the artifact to download.")
 @click.option('--save-as',required = True , help="Save the fetched object to Minio as the given file name.")
-def download_artefact(config, client_id, artefact_url, save_as):
-    """Download the artefact."""
-    download_features(config, client_id, artefact_url, save_as)
+def download_artifact(config, client_id, artifact_url, save_as):
+    """Download the artifact."""
+    download_features(config, client_id, artifact_url, save_as)
+
+
+@click.command()
+@click.option('--config', required=True, help="Path to the config file.")
+@click.option('--client-id', required=True, help="Client ID for authentication.")
+@click.option('--artifact-url', required=True, help="URL of the artifact to buffer.")
+@click.option('--buffer-d', required=True, help="Buffer distance.")
+@click.option('--store-artifact', help="Store the buffered artifact.")
+@click.option('--file-path', help="Path to save the buffered artifact.")
+def create_buffer(config, client_id, artifact_url, buffer_d, store_artifact, file_path):
+    """Buffer the artifact."""
+    make_buffer(config, client_id, artifact_url, buffer_d, store_artifact, file_path)
