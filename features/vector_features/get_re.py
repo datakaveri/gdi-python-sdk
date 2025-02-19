66ba6f71d61f0da8d151acecba11d19e26eba842
JoelDJ2002 <jdjferrari@gmail.com>
2025-02-19 12:47:25 +0530

    Pagination implemented in get_re.py

--- features/vector_features/get_re.py
+++ features/vector_features/get_re.py
@@ -3,6 +3,7 @@ from auth.token_gen import TokenGenerator
 from common.minio_ops import connect_store_minio
 import geopandas as gpd
 import uuid
+import io
 
 class ResourceFetcher:
     def __init__(self, client_id: str, client_secret: str, role: str):
@@ -14,19 +15,41 @@ class ResourceFetcher:
 
     def fetch_resource_data(self, resource_id:str ,save_object : bool = False,  config_path:str = None ,file_path : str = None ) -> dict:
        
+        data = []
         try:
             # Generate the token
             token_generator = TokenGenerator(self.client_id, self.client_secret,self.role)
             auth_token = token_generator.generate_token()
 
-            # Fetch resource data
-            resource_url = f"https://geoserver.dx.gsx.org.in/collections/{resource_id}/items?offset=1"
-            headers = {"Authorization": f"Bearer {auth_token}"}
-
+            # Request the collections API to get asset links 
+            resource_url = f"https://geoserver.dx.gsx.org.in/collections/{resource_id}"
+            headers = {"Authorization": f"Bearer {auth_token}"}            
             response = requests.get(resource_url, headers=headers)
             response.raise_for_status()  # Raise an HTTPError for bad responses
+                  
+            links = response.json().get("links")
+
+            # getting the rel:enclosure link to get all the data in single shot            
+            enclosure_link_arr = [link["href"] for link in links if link.get("rel") == "enclosure"]            
+            resource_url = enclosure_link_arr[0] if enclosure_link_arr else None
+
+            # iterative pagination incase if there is no rel:enclosure link
+            if resource_url is None:
+                for link in links:
+                    if link.get("rel") == "self" or link.get("rel") == "alternate" or link.get("rel") == "items":
+                        
+                        response = requests.get(link["href"], headers=headers)
+                        response.raise_for_status()
+                        data.extend(response.content)                       
+            else:
+                
+                response = requests.get(resource_url, headers=headers)
+                response.raise_for_status()
+                data.extend(response.content)
+                
+           
             
-            gdf = gpd.GeoDataFrame.from_features(response.json()['features'])
+            gdf = gpd.read_file(io.BytesIO(response.content))
 
             if save_object:
                 if not file_path:
@@ -37,6 +60,7 @@ class ResourceFetcher:
                     raise Exception(f"Error while saving file: {e}")
             else:
                 print("Data not saved. Set save_object to True , provide the minio config path and file_path to save the data to minio.")
+                print(gdf.info())
 
             return gdf  # Return the fetched data as a geopandas dataframe
         except requests.RequestException as e:
@@ -48,10 +72,10 @@ class ResourceFetcher:
 # client_id =  '7dcf1193-4237-48a7-a5f2-4b530b69b1cb' 
 # client_secret = "a863cafce5bd3d1bd302ab079242790d18cec974"
 # role = "consumer"
-# resource_id = "a4395596-14e6-4e17-83c4-989bc23fb3d2"
-# save_object = True
-# config_path = '../../config.json'
-# file_path = 'intermediate/Road_Varanasi.pkl'
+# resource_id = "024b0c51-e44d-424c-926e-254b6c966978"
+# save_object = False
+# config_path = "../config.json"
+# file_path = "intermediate/data_new3.pkl"
 # fetcher = ResourceFetcher(client_id, client_secret, role)
 # resource_data = fetcher.fetch_resource_data(resource_id, save_object, config_path, file_path)
    
