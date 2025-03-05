7f16ba9ee1d1167ef4ef4ccd0b6ec6d8dd7bb088
JoelDJ2002 <jdjferrari@gmail.com>
2025-03-05 12:50:49 +0530

    save as geojson implemented , numpy, rasterio dependecies updated

--- features/vector_features/download_features.py
+++ features/vector_features/download_features.py
@@ -8,12 +8,14 @@ from datetime import timedelta
 def download_features(config : str, client_id : str, artefact_url : str, save_as : str) -> str:
     """
     Download features from the minio bucket and save it as a geopackage file.
-    Parameters
-    ----------
+
+    Parameters:
+    ------------
     config : str (Node red will translate it as input)
     client_id : str (Node red will translate it as input)
     artefact_url : str (Node red will take it from the previous step)
-    save_as : str (Node red will translate it as input)
+    save_as : str (Node red will ignore this)
+
     """ 
     
     client = connect_minio(config, client_id)
@@ -21,17 +23,17 @@ def download_features(config : str, client_id : str, artefact_url : str, save_as
     try :
         with client.get_object(client_id, artefact_url) as response:
             data = pkl.loads(response.read())
-            data.to_file('temp.gpkg', driver='GPKG')        
+            data.to_file('temp.geojson', driver='GeoJSON')        
     except Exception as e:
         print(e)
    
     try:
-        client.fput_object( client_id,save_as,'temp.gpkg')
+        client.fput_object( client_id,save_as,'temp.geojson')
         pre_signed_url = client.get_presigned_url("GET",client_id, save_as, expires=timedelta(days=1))
-        os.remove('temp.gpkg')
+        os.remove('temp.geojson')
         print(pre_signed_url)
     except Exception as e:
         raise e
     
-# download_features(config = '../config.json', client_id = '7dcf1193-4237-48a7-a5f2-4b530b69b1cb', artefact_url = 'intermediate/data_new1.pkl', save_as = 'intermediate/features_1_temp.gpkg')
+# download_features(config = '../../config.json', client_id = '7dcf1193-4237-48a7-a5f2-4b530b69b1cb', artefact_url = 'intermediate/paginated_data.pkl', save_as = 'intermediate/features_1_temp.geojson')
 
