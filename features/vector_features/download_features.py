642256b1e2d23655562d3edc55cdbce43fd3abae
JoelDJ2002 <jdjferrari@gmail.com>
2025-04-26 16:19:48 +0530

    changed artefact to artifact

--- features/vector_features/download_features.py
+++ features/vector_features/download_features.py
@@ -6,21 +6,21 @@ import os
 import io
 from datetime import timedelta
 import uuid
-def download_features(config : str, client_id : str, artefact_url : str, save_as : str) -> str:
+def download_features(config : str, client_id : str, artifact_url : str, save_as : str) -> str:
     """
     Download features from the minio bucket and save it as a geopackage file.In editor it will be renamed as download-artifact.
     Parameters
     ----------
     config : str (Reactflow will ignore this parameter)
     client_id : str (Reactflow will translate it as input)
-    artefact_url : str (Reactflow will take it from the previous step)
+    artifact_url : str (Reactflow will take it from the previous step)
     save_as : str (Reactflow will ignore this parameter)
     """ 
     
     client = connect_minio(config, client_id)
 
     try :
-        with client.get_object(client_id, artefact_url) as response:
+        with client.get_object(client_id, artifact_url) as response:
             data = gpd.read_file(io.BytesIO(response.read()))  
             data.to_file('temp.geojson', driver='GeoJSON')        
     except Exception as e:
@@ -37,5 +37,5 @@ def download_features(config : str, client_id : str, artefact_url : str, save_as
     except Exception as e:
         raise e
     
-# download_features(config = '../../config.json', client_id = '7dcf1193-4237-48a7-a5f2-4b530b69b1cb', artefact_url = 'intermediate/paginated_data.pkl', save_as = 'intermediate/features_1_temp.geojson')
+# download_features(config = '../../config.json', client_id = '7dcf1193-4237-48a7-a5f2-4b530b69b1cb', artifact_url = 'intermediate/paginated_data.pkl', save_as = 'intermediate/features_1_temp.geojson')
 
