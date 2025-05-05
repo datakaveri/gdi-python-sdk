84079cdfda2c393bc6e431140afb8c62e2da386f
JoelDJ2002 <jdjferrari@gmail.com>
2025-05-05 09:34:36 +0530

    node-info

--- node-info-generator/node_info_generator.py
+++ node-info-generator/node_info_generator.py
@@ -3,15 +3,21 @@ import os
 import json
 import re 
 from docstring_parser import parse
-OUTPUT_JSON_PATH = 'J:/New_folder_2/gdi-python-sdk/node-info-generator/generated-info.json'
-DIRECTORY_TO_SCAN = 'J:/New_folder_2/gdi-python-sdk'
+OUTPUT_JSON_PATH = '/home/pradeep/gdi/gdi-python-sdk/node-info-generator/generated-info.json'
+DIRECTORY_TO_SCAN = '/home/pradeep/gdi/gdi-python-sdk/'
 result = []
 allAttribute=["(Reactflow will translate it as input)","(Reactflow will ignore this parameter)","(Reactflow will take it from the previous step)"]
+
 def extract_name(text):
     match = re.search(r"In editor it will be renamed as (\S+)", text)
     if match:
         return match.group(1).rstrip(".,")
     return None
+
+
+def remove_rename_pattern(text):
+    return re.sub(r"In editor it will be renamed as \S+[.,]?", "", text)
+
 def contains_any_string(target_string, string_array):
     return any(s in target_string for s in string_array)
 for root, dirs, files in os.walk(DIRECTORY_TO_SCAN):
@@ -22,6 +28,9 @@ for root, dirs, files in os.walk(DIRECTORY_TO_SCAN):
         try:
             with open(filepath, "r", encoding="utf-8") as f:
                 code = f.read()
+            featureType="raster"
+            if "vector_features" in filepath:
+                featureType ="vector"
             tree = ast.parse(code)
             for node in ast.walk(tree):
                 if isinstance(node, ast.FunctionDef):
@@ -33,6 +42,7 @@ for root, dirs, files in os.walk(DIRECTORY_TO_SCAN):
                       if len(docs.params)==0:
                             continue
                       name = extract_name(docs.description) or node.name
+                      description=remove_rename_pattern(docs.description)
                       for param in docs.params:
                         if not contains_any_string(param.type_name, allAttribute):
                           message = f"error with function {name} and with `{param.type_name}`"
@@ -40,7 +50,9 @@ for root, dirs, files in os.walk(DIRECTORY_TO_SCAN):
                       parameters={param.arg_name: param.type_name for param in docs.params}
                       result.append({
                           "nodeName": name,
-                          "inputs": parameters
+                          "description": description,
+                          "inputs": parameters,
+                          "featureType":featureType
                       })
         except (SyntaxError, UnicodeDecodeError):
             continue
