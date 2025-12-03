fc0a197b3f1bb62e802c7e229b9afca532c7e29a
Linvamp <lindatheres92@gmail.com>
2025-12-03 12:44:52 +0530

    updated get_data.py to include save artifact function so that the output file path is printed from the main function

--- features/raster_features/get_data.py
+++ features/raster_features/get_data.py
@@ -7,9 +7,54 @@ import io
 from tqdm import tqdm
 from common.minio_ops import connect_minio,stream_to_minio
 from common.convert_to_cog import tiff_to_cogtiff
-from common.save_raster_artifact import save_raster_artifact
 import warnings
 warnings.filterwarnings("ignore")
+
+import os
+import uuid
+import shutil
+import warnings
+from common.minio_ops import connect_minio, stream_to_minio
+
+warnings.filterwarnings("ignore")
+
+def save_raster_artifact(config: str, client_id: str, local_path: str, file_path: str, store_artifact: str):
+    """
+    Reprojects raster to EPSG:4326, converts to LZW-compressed COG, and saves to MinIO or local.
+    """
+    if not file_path:
+        file_path = f"processed_rasters/{uuid.uuid4()}.tif"
+
+    # Upload to minio or save locally
+    if store_artifact.lower() == "minio":
+        try:
+            client = connect_minio(config, client_id)
+            stream_to_minio(client, client_id, file_path, local_path)
+            # print(f"{file_path}")
+            aux_path = local_path + ".aux.xml"
+            if os.path.exists(aux_path):
+                stream_to_minio(client, client_id, file_path + ".aux.xml", aux_path)
+                # print(f"{file_path}.aux.xml")
+        except Exception as e:
+            raise Exception(f"Error while saving raster to MinIO: {e}")
+
+    elif store_artifact.lower() == "local":
+        try:
+            os.makedirs(os.path.dirname(file_path), exist_ok=True)
+            shutil.move(local_path, file_path)
+            print(f"Raster saved locally at: {file_path}")
+
+            aux_path = local_path + ".aux.xml"
+            if os.path.exists(aux_path):
+                shutil.move(aux_path, file_path + ".aux.xml")
+                print(f"Auxiliary XML saved locally at: {file_path}.aux.xml")
+
+        except Exception as e:
+            raise Exception(f"Error while saving raster locally: {e}")
+
+    else:
+        raise ValueError("Invalid store_artifact. Use either 'minio' or 'local'.")
+
 
 
 def get_assets(client_id: str, client_secret: str, role: str, collection_ids: str, config: str, store_artifact: str = 'minio', dir_path: str = None, item_id: str = None) -> None:
